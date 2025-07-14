087158ebc7d9a87c13247a731a188b97b9811052
Saurabh-101 <saurabhgupta72dh3@gmail.com>
2025-07-14 19:04:18 +0530

    Create nav bar and related pages

---
+++ components/Nav.jsx
@@ -0,0 +1,47 @@
+"use client";
+import React from 'react'
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+const links =[
+    {
+        name:"home",
+        path:"/"
+    },
+    {
+        name:"services",
+        path:"/services"
+    },
+    {
+        name:"resume",
+        path:"/resume"
+    },
+    {
+        name:"work",
+        path:"/work"
+    },
+    {
+        name:"contact",
+        path:"/contact"
+    },
+]
+
+
+const Nav = () => {
+    const pathName = usePathname();
+  return (
+    <nav className='flex gap-8'>
+        {
+            links.map((link,index)=>{
+                return(
+                    <Link key={link.index} className={`${link.path === pathName && "text-accent border-b-2 border-accent"} capitalize font-medium hover:text-accent transition-all`} href={link.path} >
+                        {link.name}
+                    </Link>
+                )
+            })
+        }
+    </nav>
+  )
+}
+
+export default Nav
