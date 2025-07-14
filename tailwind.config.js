3f884d25420df91c55eb19f0f88fbc414086a93c
Saurabh-101 <saurabhgupta72dh3@gmail.com>
2025-07-14 16:26:36 +0530

    Configured tailwindcss defaults and started with header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+    darkMode: ["class"],
+    content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  prefix: "",
   theme: {
-    extend: {},
+    container:{
+      center: true,
+      padding: "15px",
+    },
+    screens:{
+      sm:"640px",
+      md:"768px",
+      lg:"960px",
+      xl:"1200px",
+    },
+    fontFamily:{
+      primary: "var(--font-jetbrainsMono)",
+    },
+  	extend: {
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors:{
+        primary: "#1c1c22",
+        accent:{
+          DEFAULT:"#00ff99",
+          hover:"#00e187"
+        }
+      },
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
-
