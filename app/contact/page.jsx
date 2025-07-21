e37c112f13440905d34b70f2a84806299ca62a98
Saurabh-101 <saurabhgupta72dh3@gmail.com>
2025-07-22 00:30:56 +0530

    Contact page uder construction

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,9 +1,80 @@
-import React from 'react'
+"use client";
+
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
+
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from '@radix-ui/react-select';
+
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from 'react-icons/fa'
+
+const info = [
+  {
+    icon: <FaPhoneAlt />,
+    title: "Phone",
+    desc: "(+91) 9988747633",
+  },
+  {
+    icon: <FaEnvelope />,
+    title: "Email",
+    desc: "saurabhgupta72dh3@gmail.com",
+  },
+  {
+    icon: <FaMapMarkerAlt />,
+    title: "Address",
+    desc: "Sikanderpur, Ballia, UP, 277303",
+  },
+]
+
+import { motion } from 'framer-motion';
 
 const Contact = () => {
   return (
-    <div>contact page</div>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" }
+      }}
+      className='py-6'
+    >
+      <div className="container max-auto">
+        <div className='flex flex-col xl:flex-row gap-[30px]'>
+          {/* form */}
+          <div className='xl:h-[54%] order-2 xl:order-none'>  
+            <form className='flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl'>
+              <h3 className='text-4xl text-accent'>Let's work together</h3>
+              <p className='text-white/60'>Let’s create something amazing! Reach out for collaborations, freelance projects, or just to say hello. I’d love to hear from you!</p>
+              {/* inputs */}
+              <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
+                <Input type="firstname" placeholder="FirstName" />
+                <Input type="lastname" placeholder="LastName" />
+                <Input type="email" placeholder="Email Address" />
+                <Input type="phone" placeholder="Phone Number" />
+              </div>
+              {/* select */}
+              <Select>
+                <SelectTrigger className='w-full'>
+                  <SelectValue placeholder="Select a service"/>
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Select a service</SelectLabel>
+                    <SelectItem value='est'> Web Development </SelectItem>
+                    <SelectItem value='cst'> UI/UX Design  </SelectItem>
+                    <SelectItem value='mst'> Logo Design </SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </form>
+          </div >
+          {/* info */}
+          <div className='flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0'>info</div>
+        </div>
+      </div>
+    </motion.div>
   )
+
 }
 
 export default Contact
--- tailwind.config.js
+++ tailwind.config.js
@@ -22,11 +22,6 @@ module.exports = {
       primary: "var(--font-jetbrainsMono)",
     },
   	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
   		colors:{
         primary: "#1c1c22",
         accent:{
